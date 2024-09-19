a5545ace84eb1fb9211e8b0fa06fa53dfe0e1eb7
Scarlet Alkeyo <scarletalkeyo@gmail.com>
2024-09-19 15:35:50 +0300

    added splash screen and teaser screen

--- app/src/main/java/com/akirachix/dishhub/Teaserscreen.kt
+++ app/src/main/java/com/akirachix/dishhub/Teaserscreen.kt
@@ -6,19 +6,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.akirachix.dishhub.databinding.ActivityMainBinding
+import com.akirachix.dishhub.databinding.ActivityTeaserscreenBinding
 
-class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+class Teaserscreen : AppCompatActivity() {
+    lateinit var binding: ActivityTeaserscreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding=ActivityTeaserscreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.Logo.setOnClickListener{
-            val intent=Intent(this,Teaserscreen::class.java)
-            startActivity(intent)
-        }
-
     }
 }
