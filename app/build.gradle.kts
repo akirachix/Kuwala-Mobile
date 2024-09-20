b16cc621be2a60c1bca55800a71de73b81739179
student <chemtailodite@gmail.com>
2024-09-20 19:27:43 +0300

    added changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -42,7 +45,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("com.google.android.material:material:1.5.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    val fragment_version = "1.5.7"
+    //noinspection GradleDependency
+    implementation("androidx.fragment:fragment-ktx:$fragment_version")
 }
--- app/src/main/java/com/akirachix/dishhub/Recipes.kt
+++ app/src/main/java/com/akirachix/dishhub/Recipes.kt
@@ -1,11 +1,15 @@
 package com.akirachix.dishhub
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Recipes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_recipes)
     }
 }
